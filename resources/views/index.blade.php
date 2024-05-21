c07d9ca393c608349cd4d7fc99769a4c7a4a1efd
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-21 15:35:40 +0700

    Penambahan Asset
    Menambahkan asset yang kurang, memberikan footer , membnerikan navigation dan pembuatan page" baru. dan pembenahan routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,6 +29,20 @@
     <div class="content-two">
 
         <h1>Testimoni</h1>
+        <div class="text">
 
+            <h1>Digitalize your Bussiness, Empower your Growth with DigPro</h1>
+            <p>DIGPRO adalah mitra digital bagi UMKM, menyediakan solusi handal dengan kerjasama penyedia layanan website,
+                platform pembayaran, asosiasi UMKM, dan influencer lokal. Fokusnya adalah meningkatkan penjualan, membangun
+                reputasi merek, meningkatkan efisiensi, dan memperluas jangkauan pasar untuk UMKM, membantu mereka sukses
+                dalam era digital. </p>
+
+        </div>
+
+        <div class="image">
+
+            <img src="../../resources/images/logo.png" alt="">
+
+        </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/daftar_form.blade.php', function () {
+    return view('daftar_form');
+});
+
+Route::get('/butuh_bantuan.blade.php', function () {
+    return view('butuh_bantuan');
+});
+
+Route::get('/langganan_form.blade.php', function () {
+    return view('langganan_form');
+});
+
+Route::get('/count_page.blade.php', function () {
+    return view('count_page');
+});
